984d7b0d80d507e6eaa1e58ad2dcc7485e214ef4
oussou-dev <oussou.dev@gmail.com>
2017-11-30 16:03:00 +0100

    Consommer un service + utiliser le LoadingController

--- src/pages/accueil/accueil.ts
+++ src/pages/accueil/accueil.ts
@@ -1,7 +1,8 @@
 import { Technology } from './../../models/technology';
 import { DataService } from './../../providers/data/data.service';
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, LoadingController } from 'ionic-angular';
+
 
 @Component({
   selector: 'page-accueil',
@@ -13,12 +14,23 @@ export class AccueilPage {
 
   constructor(public navCtrl: NavController, 
               public navParams: NavParams,
-              private dataService: DataService) {
+              private dataService: DataService, 
+              private loadingCtrl: LoadingController) {
   }
 
   ionViewWillLoad() {
+    // // exemple de mise en place d'un loader
+    // const loader = this.loadingCtrl.create({
+    //   content:  'veuillez patienter svp',
+    //   duration: 2000
+    // });
+    // loader.present();
     this.technologies = this.dataService.getAllTechnologies()
-    console.log(this.technologies)
+    // console.log(this.technologies)
+    // loader.dismiss() // une fois que les donnees sont affichees
   }
+
+
+
 
 }
